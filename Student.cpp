35e216a39b73e0394b22b8a3252d9094c90f51ae
tkzc00 <zqzkzy2420586708@qq.com>
2022-09-29 11:53:27 +0800

    创建学生类

--- Student.cpp
+++ Student.cpp
@@ -1,1 +1,39 @@
 #include "Student.h"
+
+// 默认构造
+Student::Student()
+{
+}
+
+// 有参构造
+Student::Student(int id, string name, string pwd)
+{
+	this->m_Id = id;
+	this->m_Name = name;
+	this->m_Pwd = pwd;
+}
+
+//菜单界面
+void Student::openMenu()
+{
+}
+
+// 申请预约
+void Student::applyOrder()
+{
+}
+
+// 查看自身预约
+void Student::showMyOrder()
+{
+}
+
+// 查看所有预约
+void Student::showAllOrder()
+{
+}
+
+// 取消预约
+void Student::cancleOrder()
+{
+}
--- Student.h
+++ Student.h
@@ -3,8 +3,32 @@
 #include"Identity.h"
 using namespace std;
 
+// 学生类
 class Student :public Identity
 {
+	// 默认构造
+	Student();
 
+	// 有参构造
+	Student(int id, string name, string pwd);
+
+	//菜单界面
+	virtual void openMenu();
+
+	// 申请预约
+	void applyOrder();
+
+	// 查看自身预约
+	void showMyOrder();
+
+	// 查看所有预约
+	void showAllOrder();
+
+	// 取消预约
+	void cancleOrder();
+
+private:
+	// 学生学号
+	int m_Id;
 };
 
