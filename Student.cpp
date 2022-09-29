5a9040173faad68faae09bab6eb45a3b2e92de67
tkzc00 <zqzkzy2420586708@qq.com>
2022-09-29 20:44:31 +0800

    学生显示自身所有预约功能实现

--- Student.cpp
+++ Student.cpp
@@ -163,6 +163,46 @@ void Student::showMyOrder()
 // 查看所有预约
 void Student::showAllOrder()
 {
+	OrderFile of;
+	if (of.m_Size == 0)
+	{
+		cout << "无预约记录" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+
+	for (int i = 0; i < of.m_Size; i++)
+	{
+		cout << i + 1 << "、 ";
+
+		cout << "预约日期： 周" << of.m_orderData[i]["date"];
+		cout << " 时段：" << (of.m_orderData[i]["interval"] == "1" ? "上午" : "下午");
+		cout << " 学号：" << of.m_orderData[i]["stuId"];
+		cout << " 姓名：" << of.m_orderData[i]["stuName"];
+		cout << " 机房：" << of.m_orderData[i]["roomId"];
+		string status = " 状态： ";  // 0 取消的预约   1 审核中   2 已预约 -1 预约失败
+		if (of.m_orderData[i]["status"] == "1")
+		{
+			status += "审核中";
+		}
+		else if (of.m_orderData[i]["status"] == "2")
+		{
+			status += "预约成功";
+		}
+		else if (of.m_orderData[i]["status"] == "-1")
+		{
+			status += "审核未通过，预约失败";
+		}
+		else
+		{
+			status += "预约已取消";
+		}
+		cout << status << endl;
+	}
+
+	system("pause");
+	system("cls");
 }
 
 // 取消预约
