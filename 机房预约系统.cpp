26921c8b072d0a2662d3d50726b494e0fcb1b97a
tkzc00 <zqzkzy2420586708@qq.com>
2022-09-29 11:30:25 +0800

    主界面菜单搭建以及提供登录接口

--- 机房预约系统.cpp
+++ 机房预约系统.cpp
@@ -3,6 +3,47 @@ using namespace std;
 
 int main()
 {
+	// 接收用户的选择
+	int select = 0;
+
+	while (true)
+	{
+
+		cout << "======================  欢迎来到传智播客机房预约系统  =====================" << endl;
+		cout << endl << "请输入您的身份" << endl;
+		cout << "\t\t -------------------------------\n";
+		cout << "\t\t|                               |\n";
+		cout << "\t\t|          1.学生代表           |\n";
+		cout << "\t\t|                               |\n";
+		cout << "\t\t|          2.老    师           |\n";
+		cout << "\t\t|                               |\n";
+		cout << "\t\t|          3.管 理 员           |\n";
+		cout << "\t\t|                               |\n";
+		cout << "\t\t|          0.退    出           |\n";
+		cout << "\t\t|                               |\n";
+		cout << "\t\t -------------------------------\n";
+		cout << "输入您的选择: ";
+
+		cin >> select; //接受用户选择
+
+		switch (select)
+		{
+		case 1:  //学生身份
+			break;
+		case 2:  //老师身份
+			break;
+		case 3:  //管理员身份
+			break;
+		case 0:  //退出系统
+			break;
+		default:
+			cout << "输入有误，请重新选择！" << endl;
+			system("pause");
+			system("cls");
+			break;
+		}
+
+	}
 	system("pause");
 	return 0;
 }
